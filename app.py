45b5ddffb94e16f98b9bd2a8cee045d9ad69a657
Tingk28 <alex900428@gamil.com>
2023-08-31 18:27:20 +0800

    auto update

--- app.py
+++ app.py
@@ -22,6 +22,71 @@ try:
 except Exception as e:
     print(f"發生錯誤{e}")
 
+import threading
+import time
+import requests
+from bs4 import BeautifulSoup
+
+def update():
+    print("程式已執行！")
+    
+    url = "http://www.9800.com.tw/lotto539/statistics.html"  # 替換成你要爬取的網頁的URL
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # 檢查是否有錯誤的回應
+
+        # 如果回應狀態碼是 200 OK，則解析網頁
+        if response.status_code == 200:
+            response.encoding = "utf-8"  # 替換成適合的編碼
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            # 使用 Beautiful Soup 提取元素
+            # 使用 CSS 選擇器選取元素
+            tbody_elements = soup.select('table tbody')
+            new_data = []
+            target_rows = tbody_elements[2].find_all('tr')
+            for row in target_rows:
+                row_data = []
+                td_elements = row.select('td')
+                for td in td_elements:
+                    text = td.text.replace("\n","").replace("\xa0","")
+                    row_data.append(text)
+                new_data.append(row_data)
+            print(new_data)
+
+
+    except requests.exceptions.RequestException as e:
+        print("發生錯誤:", e)
+    
+    new_rows = []
+    for new_row in new_data:
+        new_row_exists = any(new_row == row for row in lines)
+
+        # 如果新行不存在，則將其寫入到 CSV 的最後
+        if not new_row_exists:
+            new_rows.append(new_row)
+
+    # 寫入新行到 history.csv
+    if new_rows:
+        with open("history.csv", 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerows(new_rows)
+        print("已寫入新行到 history.csv")
+    else:
+        print("所有資料均已存在")
+
+def schedule_thread():
+    while True:
+        now = time.localtime()
+        if now.tm_hour == 20 and now.tm_min >= 35:
+            update()
+        time.sleep(60)  # 每隔一分鐘檢查一次
+
+# 創建一個執行緒來執行 schedule_thread 函數
+thread = threading.Thread(target=schedule_thread)
+thread.daemon = True  # 設定為守護執行緒，程式結束時自動停止執行緒
+thread.start()
 
 @app.route("/callback", methods=['POST'])
 def callback():
