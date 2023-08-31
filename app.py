85c3873aec867295eac6e31cb112b825e2423266
Tingk28 <alex900428@gamil.com>
2023-08-31 11:56:33 +0800

    return_pair 1

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
+try:
+    with open("history.csv", 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        lines = list(reader)
+    except Exception as e:
+        print(f"發生錯誤{e}")
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -30,25 +38,34 @@ def handle_message(event):
     user_message = event.message.text
     print(user_message)
     reply_message = user_message
-    if user_message == "最近":
+    if reply_message == "最近":
         reply_message = return_recent()
+    else :
+        user_message = user_message.replace(","," ").split()
+        if len(user_message)>1:
+            reply_message = return_pair(user_message)
+            
+            
+            
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_message)
     )
 def return_recent():
     try:
-        with open("history.csv", 'r', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            lines = list(reader)
-            result = lines[-5:]
-            reply_message = ""
-            for row in result:
-                reply_message += row[1] +"("+ row[0] +"期)\n"
-                reply_message += row[2] + ","+row[3] + ","+row[4] + ","+row[5] + ","+row[6]+"\n"
-            return reply_message
+        result = lines[-5:]
+        reply_message = ""
+        for row in result:
+            reply_message += row[1] +"("+ row[0] +"期)\n"
+            reply_message += row[2] + ","+row[3] + ","+row[4] + ","+row[5] + ","+row[6]+"\n"
+        return reply_message
     except Exception as e:
-        return f"發生錯誤{e}"
+        return f"發生錯誤"
+
+def return_pair(pair):
+    return str(pair)
+
+
 
 
 if __name__ == "__main__":
